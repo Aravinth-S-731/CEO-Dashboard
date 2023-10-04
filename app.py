bd232789ac205299561da281382be030d4e3bfd8
Aravinth-S-731 <aravinth7871867225@gmail.com>
2023-10-05 00:19:11 +0530

    05/10/2023 Morning Progress

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.secret_key = 'r$W9#kLp2&QnX@5*8yZ$'
 
-# Enter your database connection details below
+# Database connection
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '@Arvi7777'
@@ -16,10 +16,8 @@ app.config['MYSQL_DB'] = 'ceo-application'
 # Initialize MySQL
 mysql = MySQL(app)
 
-# http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
@@ -39,15 +37,58 @@ def login():
             session['id'] = ceo_login_database['id']
             session['username'] = ceo_login_database['username']
             # Redirect to home page
-            msg = 'Logged in successfully!'
-            print(msg)
-            return 'Logged in successfully!'
+            return redirect (url_for('landingPage'))
         else:
-            # Account doesnt exist or username/password incorrect
+            # username/password incorrect
             msg = 'Incorrect username/password!'
-            print(msg)
-
     return render_template('login.html', msg='')
 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect (url_for('login'))
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'mail' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        mail = request.form['mail']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM `ceo_login_database` WHERE username = %s', (username,))
+        ceo_login_database = cursor.fetchone()
+        # If ceo_login_database exists show error and validation checks
+        if ceo_login_database:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', mail):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not mail:
+            msg = 'Please fill out the form!'
+        else:
+            # Hash the password
+            hash = password + app.secret_key
+            hash = hashlib.sha1(hash.encode())
+            password = hash.hexdigest()
+            # ceo_login_database doesn't exist, and the form data is valid, so insert the new ceo_login_database into the ceo_login_databases table
+            cursor.execute('INSERT INTO `ceo_login_database` VALUES (NULL, %s, %s, %s)', (username, password, mail,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+            return redirect(url_for('login'))
+    elif request.method == 'POST':
+        msg = 'Incomplete Form'
+    return render_template('register.html', msg = msg)
+
+@app.route('/home/landing-page')
+def landingPage():
+    if 'loggedin' in session:
+        return render_template('landing_page.html', username = session['username'])
+    return redirect(url_for('login'))
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ static/javascript/App.js
@@ -0,0 +1,7 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.top = "0";
+}
+function hideMenu(){
+    navLinks.style.top = "-200px";
+}
